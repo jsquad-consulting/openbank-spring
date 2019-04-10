d1659875361869db494e80deefd1b23d990d4f7d
Andy Holst <andy@jsquad.se>
2019-04-10 02:29:06 +0200

    Refatored code
    Refactored StartupOpenBankComponentImpl to only populate database if
    Mr. Spock test client  does not exist.
    
    Refactord EntityGeneratorImpl to load property mock data from
    client.properties file when generating client list.
    
    Updated AppProperty to inject all property field with help of
    property setup annotation.
    
    Updated test cases when retrieving a new generated client acting as
    'prototype scope'.
    
    Cleaned up applicationContext.xml to only setup JPA and transaction
    vendor.

--- src/main/java/se/jsquad/generator/EntityGeneratorImpl.java
+++ src/main/java/se/jsquad/generator/EntityGeneratorImpl.java
@@ -4,20 +4,29 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import se.jsquad.entity.Client;
+import se.jsquad.entity.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Service("entityGeneratorImpl")
+@PropertySource("classpath:/META-INF/property/client.properties")
 public class EntityGeneratorImpl implements EntityGenerator {
     private static final Logger logger = LogManager.getLogger(EntityGeneratorImpl.class.getName());
 
+    private Environment environment;
+
     @Autowired
-    private ApplicationContext applicationContext;
+    private EntityGeneratorImpl(Environment environment) {
+        logger.log(Level.INFO, "EntityGeneratorImpl(environment: {}",
+                environment);
+        this.environment = environment;
+    }
 
     @Override
     public List<Client> generateClientList() {
@@ -25,8 +34,11 @@ public class EntityGeneratorImpl implements EntityGenerator {
 
         List<Client> clientList = new ArrayList<>();
 
-        Client client = (Client) applicationContext.getBean("client");
+        Client client = new Client();
+        client.setPerson(new Person());
         client.getPerson().setClient(client);
+        client.getPerson().setFirstName(environment.getProperty("person.firstName"));
+        client.getPerson().setPersonIdentification(environment.getProperty("person.identification"));
 
         clientList.add(client);
 
--- src/main/java/se/jsquad/property/AppPropertyImpl.java
+++ src/main/java/se/jsquad/property/AppPropertyImpl.java
@@ -4,10 +4,15 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+@Component("appProperty")
+@PropertySource("classpath:/META-INF/property/app.properties")
 public class AppPropertyImpl implements AppProperty {
     private static final Logger logger = LogManager.getLogger(AppPropertyImpl.class.getName());
 
+    @Value("${app.version}")
     private String version;
 
     @Value("${app.name}")
