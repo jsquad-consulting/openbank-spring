01120323c766fabc4dfd370d43a266bb5110fc92
Andy Holst <andy@jsquad.se>
2019-06-04 00:51:28 +0200

    Added JMS send queue test case
    Added simple JMS queue integration test that tests sending a simple
    string message on the destination queue.
    
    Added embedded active mq annotation configuration to be able to test
    sending a message over the destination queue.
    
    Added JMS api pom dependency cause Spring needs it.
    
    Implemented the JmsQueueSender and MessageListener.
    
    Added bean annotation configuration for the queue destination,
    jmsTemplate, jmsContainer listener, connectionFactory and
    activeMqConnectionFactory and the jmsContainer message listener.

---
+++ src/test/java/se/jsquad/jms/JmsContainerIntegrationTest.java
@@ -0,0 +1,35 @@
+package se.jsquad.jms;
+
+import org.apache.activemq.broker.BrokerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import se.jsquad.configuration.ApplicationConfiguration;
+
+import javax.jms.MessageListener;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ApplicationConfiguration.class, loader = AnnotationConfigContextLoader.class)
+public class JmsContainerIntegrationTest {
+    @Autowired
+    private JmsQueueSender jmsQueueSender;
+
+    @Autowired
+    @Qualifier("jmsMessageListener")
+    private MessageListener messageListener;
+
+    @Autowired
+    @Qualifier("broker")
+    private BrokerService brokerService;
+
+
+    @Test
+    public void testContainerIntegrationTest() {
+        // When
+        jmsQueueSender.sendMessage("Hello destination queue");
+    }
+}
