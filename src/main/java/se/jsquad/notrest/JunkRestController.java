c063b92c0d10f69111c4a811a288fa6b03a9f84f
Andy Holst <andy@jsquad.se>
2019-09-29 12:54:54 +0200

    Added annotations for excluding spec. REST calls
    Added new api logic that verifies that hidden RESTful
    operations don't get documented in the YAML OpenApi v3
    specification file.
    
    Refactored GetClientInformationRestController so the body
    content is class specific.
    
    Added new JunkRestController that has hidden operation set.

---
+++ src/main/java/se/jsquad/notrest/JunkRestController.java
@@ -0,0 +1,31 @@
+package se.jsquad.notrest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.jsquad.client.info.JunkApi;
+
+@RestController
+@RequestMapping(path = "/api")
+public class JunkRestController {
+    private Logger logger;
+
+    public JunkRestController(Logger logger) {
+        logger.log(Level.INFO, "GetClientInformationREST(logger: {})",
+                logger);
+        this.logger = logger;
+    }
+
+    @GetMapping(value = "/junk/info/test/{personIdentification}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(hidden = true)
+    public ResponseEntity<JunkApi> getJunkInformation(@PathVariable String junkInformation) {
+
+        return ResponseEntity.ok(new JunkApi());
+    }
+}
