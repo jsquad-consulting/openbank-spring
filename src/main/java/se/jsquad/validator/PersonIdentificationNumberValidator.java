3e8247c45b35c1f6764cd34ebf7cf455acf587a4
Andy Holst <andy@jsquad.se>
2019-09-29 19:32:31 +0200

    Added PersonIdentificationNumberConstraint logic
    Added PersonIdentificationNumberConstraint logic to the GET RESTful
    client information endpoint parameter.
    
    Moved the JunkRestController to the rest package.
    
    Updated the RestResponseEntityExceptionHandler to deal with
    ConstraintViolationException as bad request status REST code.
    
    Added PersonIdentificationNumberValidator implementation of the
    PersonIdentificationNumberConstraint with default message, that
    the string must be twelve digits.
    
    Updated the get client information to test the person
    identification number as null, less then twelve digits and empty
    for ConstraintViolationException and mvc mock for system tests.
    
    Updated the integration test body content expected message due to
    the new constraint vioaltion exception logic for personal
    identification number.

---
+++ src/main/java/se/jsquad/validator/PersonIdentificationNumberValidator.java
@@ -0,0 +1,13 @@
+package se.jsquad.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PersonIdentificationNumberValidator
+        implements ConstraintValidator<PersonIdentificationNumberConstraint, String> {
+    @Override
+    public boolean isValid(String personIdentificationNumber, ConstraintValidatorContext constraintValidatorContext) {
+        return !(personIdentificationNumber == null || personIdentificationNumber.isEmpty()
+                || !personIdentificationNumber.matches("[0-9]{12}"));
+    }
+}
