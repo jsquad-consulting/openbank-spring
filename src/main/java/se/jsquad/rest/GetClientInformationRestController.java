14d540aa84d5d3856f5b868a393c9cb95eeb639b
Andy Holst <andy@jsquad.se>
2019-11-08 00:40:03 +0100

    Added Java annotated WebAppInitializer
    Removed the web.xml initializer and replaced it with the annotated
    Java WebAppInitializer.
    
    Resolved maven pom dependicy conflicts. Updated the
    ApplicationConfiguration class so that web mvc is enabled.

--- src/main/java/se/jsquad/rest/GetClientInformationRestController.java
+++ src/main/java/se/jsquad/rest/GetClientInformationRestController.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import se.jsquad.business.OpenBankService;
 import se.jsquad.client.info.ClientApi;
 
-@Controller("getClientInformationRestController")
+@RestController("getClientInformationRestController")
 @RequestMapping(path = "/api")
 public class GetClientInformationRestController {
     private Logger logger;
