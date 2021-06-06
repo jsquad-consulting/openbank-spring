6e321eb44bc67df77d94608a5a6d893934d9cfff
Andy Holst <andy.holst85@gmail.com>
2021-06-06 14:26:30 +0200

    Improved JaCoCo coverage logic
    Replaced JaCoCo check coverage profile with a script logic instead
    since the JaCoCo check coverage execution is unreliable with
    verifying the code coverage for a potential none existent file.
    
    Cleaned up pom files of unnecessary maven surefire dependency in
    service module.
    
    Added a new coverage module that aggregates all reports of all
    provided submodules to the same xml report. Will not be included in
    the packaging build directory.
    
    Improved the check coverage stage pipeline stage to be more sensitive
    to failure of generating the JaCoCo xml report directory.
    
    Cleaned up unnecessary integration test maven configurations.
    
    Added new generate integration coverage and new generate JaCoCo xml
    report profiles.
    
    Added JaCoCo configuration to exclude all generated JAXB API classes
    for code coverage.

---
+++ verify_code_coverage.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# Copyright 2021 JSquad AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+main() {
+	minimum_coverage_percentage=$1
+
+	find ./target/site -name "jacoco.xml" | grep .
+
+	if [[ $? -ne 0 ]]; then
+		echo "Failed to generate JaCoCo xml report!"
+		exit 1
+	fi
+
+	regex='<counter type="INSTRUCTION" missed="([0-9]+)" covered="([0-9]+)"\/>'
+
+	content=$(cat ./target/site/jacoco.xml | xmllint --format - | grep -i '<counter type="INSTRUCTION" missed="' | tail -1)
+
+	if [[ $content =~ $regex ]]; then
+		missed_instructions="${BASH_REMATCH[1]}"
+		covered_instructions="${BASH_REMATCH[2]}"
+	fi
+
+	total_instructions=$(echo "$missed_instructions+$covered_instructions" | bc)
+	covered_instructions=$(echo "scale=4; $covered_instructions/$total_instructions" | bc)
+	covered_instructions_in_percentage=$(echo "$covered_instructions*100" | bc)
+
+	echo "Code coverage is at $covered_instructions_in_percentage percent."
+
+	if (( $(echo "$covered_instructions_in_percentage < $minimum_coverage_percentage" | bc -l) )); then
+		echo "Coverage is less then $minimum_coverage_percentage percent."
+		exit 1
+	fi
+}
+main $*
